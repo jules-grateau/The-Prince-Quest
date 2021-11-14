70576be24912aa347da4f6a24a17dbefb9643953
Jules Grateau <jules.grateau@gmail.com>
2021-11-14 17:00:42 +0100

    Fixing CameraController to find references to Player and CameraBoundaries without any drag and drop references.

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -3,40 +3,35 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public string PlayerGameObjectPath = "Player";
+    public string CameraBoundariesGameObjectPath = "CameraRail/CameraBoundaries";
     CinemachineVirtualCamera virtualCamera;
     CinemachineConfiner cinemachineConfiner;
-    EventManager eventManager;
     // Start is called before the first frame update
 
     void Awake()
     {
-        Debug.Log("Awake camera n"+gameObject.GetInstanceID());
         LoadCamera();
     }
 
 
     void LoadCamera()
     {
-        eventManager = EventManager.current;
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
-        GameObject player = GameObject.Find("Player");
+        Transform parent = transform.parent.transform;
+        Transform player = parent.Find(PlayerGameObjectPath);
+        
         if (player != null)
-            virtualCamera.Follow = player.transform;
-        else
-            Debug.Log(" camera n" + gameObject.GetInstanceID() + " could find player");
+        {
+            virtualCamera.Follow = player;
+        }
 
         cinemachineConfiner = GetComponent<CinemachineConfiner>();
-        GameObject cameraBoundaries = GameObject.Find("CameraBoundaries");
+        Transform cameraBoundaries = parent.Find(CameraBoundariesGameObjectPath);
         if (cameraBoundaries != null)
         {
             cinemachineConfiner.m_BoundingShape2D = cameraBoundaries.GetComponent<Collider2D>();
         }
-        else
-            Debug.Log(" camera n" + gameObject.GetInstanceID() + " could find boundaries");
-    }
 
-    private void OnDestroy()
-    {
-        Debug.Log("Destroying camera N" + gameObject.GetInstanceID());
     }
 }
